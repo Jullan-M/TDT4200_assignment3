440c3ada79b4a7ad5fdcf7ed21f47cb9941c6b01
jullan <jmihkkal_lindi@hotmail.com>
2019-09-25 18:42:01 +0200

    Last commit for the day.

--- main.c
+++ main.c
@@ -85,6 +85,33 @@ void applyKernel(unsigned char **out, unsigned char **in, unsigned int width, un
     }
 }
 
+void applyKernelPrivately(unsigned char **out, unsigned char **in, unsigned int width, unsigned int height, int *kernel, unsigned int kernelDim, float kernelFactor) {
+    unsigned int const kernelCenter = (kernelDim / 2);
+    unsigned int const kernelRadi = (kernelDim - 1) / 2;
+    for (unsigned int y = kernelRadi; y < height - kernelRadi; y++) {
+        for (unsigned int x = 0; x < width; x++) {
+            int aggregate = 0;
+            for (unsigned int ky = 0; ky < kernelDim; ky++) {
+                int nky = kernelDim - 1 - ky;
+                for (unsigned int kx = 0; kx < kernelDim; kx++) {
+                    int nkx = kernelDim - 1 - kx;
+
+                    int yy = y + (ky - kernelCenter);
+                    int xx = x + (kx - kernelCenter);
+                    if (xx >= 0 && xx < (int) width && yy >=0 && yy < (int) height)
+                        aggregate += in[yy][xx] * kernel[nky * kernelDim + nkx];
+                }
+            }
+            aggregate *= kernelFactor;
+            if (aggregate > 0) {
+                out[y][x] = (aggregate > 255) ? 255 : aggregate;
+            } else {
+                out[y][x] = 0;
+            }
+        }
+    }
+}
+
 
 void help(char const *exec, char const opt, char const *optarg) {
     FILE *out = stdout;
@@ -228,9 +255,9 @@ int main(int argc, char **argv) {
         displs[i] = data_counts[i] * i;
 
         // Last rank gets the remainder rows if they don't divide evenly.
-        if (i == comm_sz - 1 && i != 0) {
+        if (i == comm_sz - 1) {
             data_counts[i] += (im_YSZ % comm_sz) * local_XSZ;
-            displs[i] += data_counts[i] - data_counts[i - 1];
+            displs[i] += data_counts[i] - data_counts[0];
         }
     }
 
